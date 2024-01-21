64b5b15c6ba51bd2d1819786c3c9e21f276311eb
Jalen Garnett <jgarnett18@gmail.com>
2024-01-21 17:48:31 -0500

    added tests

---
+++ src/test/java/org/fhirpath/aliases/ExtensionValueCastPathAliasTest.java
@@ -0,0 +1,28 @@
+package org.fhirpath.aliases;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExtensionValueCastPathAliasTest {
+
+    @Test
+    public void testExtensionPathAliasPattern() {
+        ExtensionValueCastPathAlias alias = new ExtensionValueCastPathAlias();
+        String path = "extension(testUrl).valueCodeableConcept";
+        String path2 = "extension.valueString";
+        Assertions.assertTrue(alias.getPattern().matcher(path).find());
+        Assertions.assertTrue(alias.getPattern().matcher(path2).find());
+    }
+
+    @Test
+    public void testExtensionPathAliasMutator() {
+        ExtensionValueCastPathAlias alias = new ExtensionValueCastPathAlias();
+        String path = "extension(testUrl).valueCodeableConcept";
+        String path2 = "extension.valueString";
+        String output = alias.getMutator().apply(path);
+        String output2 = alias.getMutator().apply(path2);
+        Assertions.assertEquals("extension(testUrl).value", output);
+        Assertions.assertEquals("extension.value", output2);
+    }
+
+}
